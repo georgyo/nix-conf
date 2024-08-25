b1ebf9f205325f79111d5df46d65e218da0ec85a
George Shammas <george@shamm.as>
2024-08-25 17:27:15 -0400

    Forgejo setup

--- hosts/hydra/containers/forge.nix
+++ hosts/hydra/containers/forge.nix
@@ -1,4 +1,3 @@
-
 { config, pkgs, ... }:
 {
   containers.forge = {
@@ -15,8 +14,49 @@
 
         services.forgejo = {
           enable = true;
-          settings.server.DOMAIN = "forge.scalable.io";
+          package = pkgs.forgejo;
+          database.type = "postgres";
+          settings.server = {
+            DOMAIN = "forge.scalable.io";
+            ROOT_URL = "https://forge.scalable.io";
+          };
+          settings.service = {
+            DISABLE_REGISTRATION = true;
+          };
         };
+
+        networking.firewall.allowedUDPPorts = [ 443 ];
+
+        services.openssh = {
+          enable = true;
+          settings = {
+            PasswordAuthentication = false;
+            PermitRootLogin = "no";
+            AllowUsers = [ "forgejo" ];
+          };
+        };
+
+        services.nginx = {
+          enable = true;
+          package = pkgs.nginxQuic;
+          recommendedProxySettings = true;
+
+          virtualHosts = {
+            "forge.scalable.io" = {
+
+              serverAliases = [ ];
+              forceSSL = true;
+              enableACME = true;
+              quic = true;
+              locations."/".proxyPass = "http://127.0.0.1:3000";
+              default = true;
+              extraConfig = ''
+                client_max_body_size 512M;
+              '';
+            };
+          };
+        };
+
       };
   };
 }
