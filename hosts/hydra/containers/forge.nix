1e07b4688c6a3ce2c00853f2f2fb97fc8c6c74f9
George Shammas <george@shamm.as>
2024-08-25 20:58:35 -0400

    Forgejo without middlemen

--- hosts/hydra/containers/forge.nix
+++ hosts/hydra/containers/forge.nix
@@ -12,51 +12,61 @@
         nixpkgs.pkgs = pkgs;
         imports = [ (import ./common.nix "forge") ];
 
+        networking.firewall.allowedUDPPorts = [ 443 ];
+        networking.firewall.allowedTCPPorts = [
+          2222
+          3000
+          3080
+        ];
+        networking.firewall.extraCommands = ''
+          iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 22 -j REDIRECT --to-ports 2222
+          ip6tables -t nat -A PREROUTING -i eth0 -p tcp --dport 22 -j REDIRECT --to-ports 2222
+
+          iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 80 -j REDIRECT --to-ports 3080
+          ip6tables -t nat -A PREROUTING -i eth0 -p tcp --dport 80 -j REDIRECT --to-ports 3080
+
+          iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 443 -j REDIRECT --to-ports 3000
+          ip6tables -t nat -A PREROUTING -i eth0 -p tcp --dport 443 -j REDIRECT --to-ports 3000
+
+        '';
+
         services.forgejo = {
           enable = true;
           package = pkgs.forgejo;
           database.type = "postgres";
-          settings.server = {
-            DOMAIN = "forge.scalable.io";
-            ROOT_URL = "https://forge.scalable.io";
-          };
-          settings.service = {
-            DISABLE_REGISTRATION = true;
-          };
-        };
-
-        networking.firewall.allowedUDPPorts = [ 443 ];
-
-        services.openssh = {
-          enable = true;
           settings = {
-            PasswordAuthentication = false;
-            PermitRootLogin = "no";
-            AllowUsers = [ "forgejo" ];
-          };
-        };
 
-        services.nginx = {
-          enable = true;
-          package = pkgs.nginxQuic;
-          recommendedProxySettings = true;
+            server = {
+              DOMAIN = "forge.scalable.io";
+              ROOT_URL = "https://forge.scalable.io";
 
-          virtualHosts = {
-            "forge.scalable.io" = {
+              PROTOCOL = "https";
+              REDIRECT_OTHER_PORT = true;
+              PORT_TO_REDIRECT = 3080;
+              ENABLE_ACME = true;
+              ACME_ACCEPTTOS = true;
+              ACME_EMAIL = "acme@shamm.as";
 
-              serverAliases = [ ];
-              forceSSL = true;
-              enableACME = true;
-              quic = true;
-              locations."/".proxyPass = "http://127.0.0.1:3000";
-              default = true;
-              extraConfig = ''
-                client_max_body_size 512M;
-              '';
+              START_SSH_SERVER = true;
+              SSH_LISTEN_PORT = 2222;
+              BUILTIN_SSH_SERVER_USER = "git";
+
+            };
+            service = {
+              DISABLE_REGISTRATION = true;
+            };
+            cache.ADAPTER = "twoqueue";
+            mailer = {
+              ENABLED = true;
+              FROM = "forgejo@scalable.io";
+              PROTOCOL = "smtp";
+              SMTP_ADDR = "192.168.55.1";
+              SMTP_PORT = "25";
+
             };
           };
         };
-
       };
+
   };
 }
