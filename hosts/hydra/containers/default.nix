d6910a7f8d2eb3c231dc81c8a401f4b0913221ed
George Shammas <george@shamm.as>
2024-09-13 08:49:20 -0400

    Virt bridge routes are handled by bird

--- hosts/hydra/containers/default.nix
+++ hosts/hydra/containers/default.nix
@@ -26,41 +26,41 @@
       }
     ];
     bridgeConfig.ProxyARP = "yes";
-    routes = [
-      {
-        Destination = "172.104.15.252";
-        Scope = "link";
-      }
-      {
-        Destination = "23.239.10.144";
-        Scope = "link";
-      }
-      {
-        Destination = "23.239.10.184";
-        Scope = "link";
-      }
-      {
-        Destination = "23.239.9.39";
-        Scope = "link";
-      }
-      {
-        Destination = "198.74.56.101";
-        Scope = "link";
-      }
-      {
-        Destination = "66.228.36.99";
-        Scope = "link";
-      }
-
-      {
-        Destination = "192.168.55.0/24";
-        Scope = "link";
-      }
-      {
-        Destination = "2600:3c03:e002:2500::/56";
-        Scope = "link";
-      }
-    ];
+#    routes = [
+#      {
+#        Destination = "172.104.15.252";
+#        Scope = "link";
+#      }
+#      {
+#        Destination = "23.239.10.144";
+#        Scope = "link";
+#      }
+#      {
+#        Destination = "23.239.10.184";
+#        Scope = "link";
+#      }
+#      {
+#        Destination = "23.239.9.39";
+#        Scope = "link";
+#      }
+#      {
+#        Destination = "198.74.56.101";
+#        Scope = "link";
+#      }
+#      {
+#        Destination = "66.228.36.99";
+#        Scope = "link";
+#      }
+#
+#      {
+#        Destination = "192.168.55.0/24";
+#        Scope = "link";
+#      }
+#      {
+#        Destination = "2600:3c03:e002:2500::/56";
+#        Scope = "link";
+#      }
+#    ];
   };
 
   imports = [
