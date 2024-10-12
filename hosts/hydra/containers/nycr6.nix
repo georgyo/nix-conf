11e3e7d7c8e680cf046bfba50eabdd101a076bce
George Shammas <george@shamm.as>
2024-10-12 08:34:14 -0400

    trust cloudflare ips

--- hosts/hydra/containers/nycr6.nix
+++ hosts/hydra/containers/nycr6.nix
@@ -1,4 +1,9 @@
-{ config, pkgs, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 
 let
   phpPackage = pkgs.php.buildEnv {
@@ -79,6 +84,8 @@ in
               DirectoryIndex index.php index.html
             '';
 
+            extraModules = [ "remoteip" ];
+
             virtualHosts.wiki = {
               hostName = "wiki.nycresistor.com";
               enableACME = true;
@@ -89,19 +96,49 @@ in
                   alias = "/srv/http/wiki.nycresistor.com/w/index.php";
                 };
               };
+              extraConfig = lib.strings.concatStrings [
+                ''
+                  <Directory "/srv/http/wiki.nycresistor.com">
+                    AllowOverride All
+                    DirectoryIndex index.php index.html
+                    Options -Indexes +FollowSymLinks
+                    Require all granted
+                  </Directory>
 
-              extraConfig = ''
-                <Directory "/srv/http/wiki.nycresistor.com">
-                  AllowOverride All
-                  DirectoryIndex index.php index.html
-                  Options -Indexes +FollowSymLinks
-                  Require all granted
-                </Directory>
-
-                RewriteEngine  on
-                RewriteRule    ^/$ /wiki [R]
-
-              '';
+                  RewriteEngine  on
+                  RewriteRule    ^/$ /wiki [R]
+                  RemoteIPHeader CF-Connecting-IP
+                ''
+                (
+                  let
+                    cloudflare_ips = [
+                      "173.245.48.0/20"
+                      "103.21.244.0/22"
+                      "103.22.200.0/22"
+                      "103.31.4.0/22"
+                      "141.101.64.0/18"
+                      "108.162.192.0/18"
+                      "190.93.240.0/20"
+                      "188.114.96.0/20"
+                      "197.234.240.0/22"
+                      "198.41.128.0/17"
+                      "162.158.0.0/15"
+                      "104.16.0.0/13"
+                      "104.24.0.0/14"
+                      "172.64.0.0/13"
+                      "131.0.72.0/22"
+                      "2400:cb00::/32"
+                      "2606:4700::/32"
+                      "2803:f800::/32"
+                      "2405:b500::/32"
+                      "2405:8100::/32"
+                      "2a06:98c0::/29"
+                      "2c0f:f248::/32"
+                    ];
+                  in
+                  lib.concatMapStrings (x: "RemoteIPTrustedProxy " + x + "\n") cloudflare_ips
+                )
+              ];
             };
 
             # virtualHosts.pass = {
