e2d68206461b9f11319166b688eef9b3c80d49b2
George Shammas <george@shamm.as>
2025-02-20 04:28:52 -0500

    nycr6: switch from httpd to nginx

--- hosts/hydra/containers/nycr6.nix
+++ hosts/hydra/containers/nycr6.nix
@@ -24,6 +24,8 @@ let
       post_max_size = 21M
     '';
   };
+
+  myConfig = config.containers.nycr6.config;
 in
 {
 
@@ -60,13 +62,21 @@ in
           lua51Packages.lua
         ];
 
-        users.users.wwwrun.home = "/var/lib/wwwrun";
+        networking.firewall.allowedUDPPorts = [ 443 ];
 
         systemd.services.httpd.path = with pkgs; [
           graphicsmagick-imagemagick-compat
           gnupg
           lua51Packages.lua
         ];
+
+        # This user is no longer used, but kept around because of file ownership.
+        users.groups.wwwrun = { };
+        users.users.wwwrun = {
+          home = "/var/lib/wwwrun";
+          isSystemUser = true;
+          group = "wwwrun";
+        };
 
         services = {
           mysql = {
@@ -78,54 +88,92 @@ in
             databases = [ "nycrmw" ];
           };
 
-          cron.enable = true;
-
-          httpd = {
-            enable = true;
-            enablePHP = true;
+          phpfpm = {
             inherit phpPackage;
-            extraConfig = ''
-              DirectoryIndex index.php index.html
-            '';
-
-            virtualHosts.wiki = {
-              hostName = "wiki.nycresistor.com";
-              enableACME = true;
-              forceSSL = true;
-              documentRoot = "/srv/http/wiki.nycresistor.com";
-              locations = {
-                "/wiki" = {
-                  alias = "/srv/http/wiki.nycresistor.com/w/index.php";
-                };
+            pools.web = {
+              user = "nginx";
+              settings = {
+                pm = "dynamic";
+                "listen.owner" = myConfig.services.nginx.user;
+                "pm.max_children" = 5;
+                "pm.start_servers" = 2;
+                "pm.min_spare_servers" = 1;
+                "pm.max_spare_servers" = 3;
+                "pm.max_requests" = 500;
               };
-              extraConfig = ''
-                <Directory "/srv/http/wiki.nycresistor.com">
-                  AllowOverride All
-                  DirectoryIndex index.php index.html
-                  Options -Indexes +FollowSymLinks
-                  Require all granted
-                </Directory>
-
-                RewriteEngine  on
-                RewriteRule    ^/$ /wiki [R]
-              '';
             };
+          };
 
-            # virtualHosts.pass = {
-            #   hostName = "pass.nycresistor.com";
-            #   enableACME = true;
-            #   forceSSL = true;
-            #   documentRoot = "/srv/http/pass.nycresistor.com/webroot";
+          nginx = {
+            enable = true;
+            package = pkgs.nginxQuic;
+            virtualHosts."wiki.nycresistor.com" = {
+              root = "/srv/http/wiki.nycresistor.com";
+              enableACME = true;
+              addSSL = true;
+              quic = true;
+              default = true;
+              extraConfig = ''
+                fastcgi_split_path_info ^(.+\.php)(/.+)$;
+                include ${pkgs.nginx}/conf/fastcgi.conf;
+                index = "index.php";
+              '';
+              locations = lib.fix (self: {
+                # Most of this matches the config from here:
+                # https://www.mediawiki.org/wiki/Manual:Short_URL/Nginx
+                "~ ^/w/(mw-config/)?(index|load|api|thumb|opensearch_desc|rest|img_auth)\\.php$" = {
+                  fastcgiParams.SCRIPT_FILENAME = "$document_root$fastcgi_script_name";
+                  extraConfig = ''
+                    fastcgi_pass unix:${myConfig.services.phpfpm.pools.web.socket};
 
-            #   extraConfig = ''
-            #     <Directory "/srv/http/pass.nycresistor.com/webroot">
-            #       AllowOverride All
-            #       DirectoryIndex index.php index.html
-            #       Options -Indexes +FollowSymLinks
-            #       Require all granted
-            #     </Directory>
-            #   '';
-            # };
+                  '';
+                };
+                "/w/images".extraConfig = ''
+                  # Separate location for images/ so .php execution won't apply
+                  # as required starting from v1.40
+                  add_header X-Content-Type-Options "nosniff";
+                  # Serve uploaded HTML as plaintext, don't execute SHTML
+                  types { text/plain html htm shtml phtml; }
+                '';
+                "~ ^/w/resources/(assets|lib|src)".extraConfig = ''
+                  try_files $uri =404;
+                  add_header Cache-Control "public";
+                  expires 7d;
+                '';
+
+                "= /favicon.ico".extraConfig = ''
+                  alias /w/images/6/64/Favicon.ico;
+                  add_header Cache-Control "public";
+                  expires 7d;
+                '';
+                "/w/rest.php/".tryFiles = "$uri $uri/ /w/rest.php?$query_string";
+
+                "~ ^/w/(skins|extensions)/.+\\.(css|js|gif|jpg|jpeg|png|svg|wasm|ttf|woff|woff2)$".extraConfig = ''
+                  try_files $uri =404;
+                  add_header Cache-Control "public";
+                  expires 7d;
+                '';
+                "/wiki/".extraConfig = ''
+                  rewrite ^/wiki/(?<pagename>.*)$ /w/index.php;
+                '';
+                "= /".return = "301 /wiki/Main_Page";
+                "= /robots.txt" = { };
+
+                # However we moved images, skins, and load.php to a different place to help with caching.
+                "/images" = self."/w/images";
+                "/skins".tryFiles = "$uri =404";
+                "/load.php" = {
+                  fastcgiParams.SCRIPT_FILENAME = "$document_root$fastcgi_script_name";
+                  extraConfig = ''
+                    fastcgi_pass unix:${myConfig.services.phpfpm.pools.web.socket};
+                  '';
+                };
+
+                # Must be the last rule
+                "/".return = "404";
+              });
+
+            };
           };
         };
       };
