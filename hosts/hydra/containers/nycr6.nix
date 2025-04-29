a2e3cc54503fa58485e097d230249fd6f1a31117
George Shammas <george@shamm.as>
2025-04-29 04:16:01 -0400

    Luasandbox is now upstream

--- hosts/hydra/containers/nycr6.nix
+++ hosts/hydra/containers/nycr6.nix
@@ -7,25 +7,6 @@
 
 let
 
-  luasandbox = pkgs.php.buildPecl rec {
-    pname = "luasandbox";
-    version = "4.1.2";
-    src = pkgs.fetchFromGitHub {
-      owner = "wikimedia";
-      repo = "mediawiki-php-luasandbox";
-      tag = version;
-      hash = "sha256-HWObytoHBvxF9+QC62yJfi6MuHOOXFbSNkhuz5zWPCY=";
-    };
-    nativeBuildInputs = [ pkgs.pkg-config ];
-    buildInputs = [ pkgs.lua51Packages.lua ];
-    meta = with lib; {
-      description = "extension for PHP 7 and PHP 8 to allow safely running untrusted Lua 5.1 code from within PHP";
-      license = licenses.mit;
-      homepage = "https://www.mediawiki.org/wiki/LuaSandbox";
-      maintainers = with lib.maintainers; [ georgyo ];
-    };
-  };
-
   phpPackage = pkgs.php.buildEnv {
     extensions = (
       { enabled, all }:
