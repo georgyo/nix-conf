cc1d2dd60b698f4ca4e919e4022499fd664f9388
George Shammas <george@shamm.as>
2025-06-24 04:56:11 -0400

    remove dead NYU servers add more coporate overloards

--- hosts/hydra/time.nix
+++ hosts/hydra/time.nix
@@ -12,17 +12,21 @@
   time.hardwareClockInLocalTime = true;
 
   networking.timeServers = [
-      "time1.google.com"
-      "time2.google.com"
-      "time3.google.com"
-      "time4.google.com"
-      "tick.nyu.edu"
-      "tock.nyu.edu"
-      "time.cloudflare.com"
-      "0.nixos.pool.ntp.org"
-      "1.nixos.pool.ntp.org"
-      "2.nixos.pool.ntp.org"
-      "3.nixos.pool.ntp.org"
+    "time.google.com"
+    "time.aws.com"
+    "time.facebook.com"
+    "time.windows.com"
+    "time.apple.com"
+    "time.nist.gov"
+    "time1.google.com"
+    "time2.google.com"
+    "time3.google.com"
+    "time4.google.com"
+    "time.cloudflare.com"
+    "0.nixos.pool.ntp.org"
+    "1.nixos.pool.ntp.org"
+    "2.nixos.pool.ntp.org"
+    "3.nixos.pool.ntp.org"
   ];
 
   services.chrony = {
