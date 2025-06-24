fc74f56ff2a868dc97ad8f6cf601fff988a88cfa
George Shammas <george@shamm.as>
2025-06-24 04:45:06 -0400

    consolidate time

--- hosts/hydra/time.nix
+++ hosts/hydra/time.nix
@@ -11,30 +11,7 @@
   time.timeZone = "America/New_York";
   time.hardwareClockInLocalTime = true;
 
-  services.chrony = {
-    enable = true;
-    servers = [
-      "time1.google.com"
-      "time2.google.com"
-      "time3.google.com"
-      "time4.google.com"
-      "tick.nyu.edu"
-      "tock.nyu.edu"
-    ];
-    extraConfig = ''
-      minsources 5
-      combinelimit 5
-      pool 0.nixos.pool.ntp.org iburst
-      pool 1.nixos.pool.ntp.org iburst
-      pool 2.nixos.pool.ntp.org iburst
-      pool 3.nixos.pool.ntp.org iburst
-      pool time.cloudflare.com iburst
-    '';
-  };
-
-  services.ntp = {
-    enable = false;
-    servers = [
+  networking.timeServers = [
       "time1.google.com"
       "time2.google.com"
       "time3.google.com"
@@ -42,7 +19,22 @@
       "tick.nyu.edu"
       "tock.nyu.edu"
       "time.cloudflare.com"
-    ];
+      "0.nixos.pool.ntp.org"
+      "1.nixos.pool.ntp.org"
+      "2.nixos.pool.ntp.org"
+      "3.nixos.pool.ntp.org"
+  ];
+
+  services.chrony = {
+    enable = true;
+    extraConfig = ''
+      minsources 5
+      combinelimit 5
+    '';
+  };
+
+  services.ntp = {
+    enable = false;
     extraConfig = ''
       statsdir /var/lib/ntp/
       statistics loopstats peerstats clockstats
