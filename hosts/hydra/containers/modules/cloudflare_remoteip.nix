6f592f22505ad39e2ff7211c404534695abf7cf1
George Shammas <george@shamm.as>
2025-09-20 05:23:58 -0400

    Nginx should also get the real up treatment

--- hosts/hydra/containers/modules/cloudflare_remoteip.nix
+++ hosts/hydra/containers/modules/cloudflare_remoteip.nix
@@ -12,4 +12,10 @@ virtualHost:
     "RemoteIPHeader CF-Connecting-IP\n"
     + (lib.strings.concatMapStrings (x: "RemoteIPTrustedProxy ${x}\n") pkgs.cloudflare_ips_v4)
     + (lib.strings.concatMapStrings (x: "RemoteIPTrustedProxy ${x}\n") pkgs.cloudflare_ips_v6);
+
+  services.nginx.appendHttpConfig =
+    "real_ip_header CF-Connecting-IP;\n"
+    + (lib.strings.concatMapStrings (x: "set_real_ip_from ${x};\n") pkgs.cloudflare_ips_v4)
+    + (lib.strings.concatMapStrings (x: "set_real_ip_from ${x};\n") pkgs.cloudflare_ips_v6);
+
 }
