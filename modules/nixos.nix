8b34d587aeba1ad166cf80480f98f494e6335ac3
George Shammas <george@shamm.as>
2026-07-12 07:48:26 -0400

    Enable distributed builds

--- modules/nixos.nix
+++ modules/nixos.nix
@@ -10,5 +10,28 @@
         # inputs.myemacs.packages.${pkgs.system}.default
         # inputs.myemacs.packages.${pkgs.system}.e
       ];
+
+      nix = {
+        buildMachines =
+          builtins.map
+            (system: {
+              hostName = "eu.nixbuild.net";
+              inherit system;
+              maxJobs = 100;
+              supportedFeatures = [
+                "benchmark"
+                "big-parallel"
+              ];
+            })
+            [
+	      "i686-linux"
+              "x86_64-linux"
+              "aarch64-linux"
+	      "armv7l-linux"
+              "aarch64-darwin"
+            ];
+
+        distributedBuilds = true;
+      };
     };
 }
